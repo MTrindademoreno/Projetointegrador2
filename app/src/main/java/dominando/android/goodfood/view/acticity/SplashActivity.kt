f4177256567c447092feebcb8470732bff5969b9
MTrindademoreno <70182644+MTrindademoreno@users.noreply.github.com>
2020-11-16 18:05:28 -0300

    splah fun timer

--- app/src/main/java/dominando/android/goodfood/view/acticity/SplashActivity.kt
+++ app/src/main/java/dominando/android/goodfood/view/acticity/SplashActivity.kt
@@ -4,23 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.security.FileIntegrityManager
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 import dominando.android.goodfood.R
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
         val img =findViewById<ImageView>(R.id.img_splash)
         Picasso.get().load("https://i.pinimg.com/originals/64/19/4c/64194cdfcded215e58a815083a0e88a6.jpg").into(img)
-        val maxSplashTime:Long = 2000
-         Handler().postDelayed({
-             startActivity(Intent(this, MainActivity::class.java))
-             finish()
+        Timer("SplashScreen", false).schedule(2000) {
+            startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+        }
 
-         },maxSplashTime
 
-         )
+
+
     }
 }
